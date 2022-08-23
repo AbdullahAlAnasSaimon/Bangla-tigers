e393885b4b69da9165620f6bc3acfbcf083637f7
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-08-23 08:58:52 +0600

    other card added

--- js/select.js
+++ js/select.js
@@ -1,10 +1,8 @@
-
-const listItems = document.getElementById('list-items');
-
 function myFunction(btnElement, titleElement){
   const selectBtn = document.getElementById(btnElement);
-  selectBtn.addEventListener('click', function(){
-  const nameTitle1 = document.getElementById(titleElement);
+  selectBtn.addEventListener('click', function clickF(){
+    const nameTitle1 = document.getElementById(titleElement);
+    const listItems = document.getElementById('list-items');
 
   if(listItems.children.length < 5){
     const li = document.createElement('li');
@@ -15,12 +13,13 @@ function myFunction(btnElement, titleElement){
     selectBtn.setAttribute('disabled', '');
     selectBtn.style.backgroundColor = 'gray';
     selectBtn.style.color = '#e8e8e8';
+    selectBtn.innerText = "SELECTED";
   }
   else if(listItems.children.length === 6){
     return;
   }
   else{
-    window.alert("You can not add more")
+    window.alert("Your Team Filled Up");
   }
 })
 }
@@ -31,3 +30,8 @@ myFunction('select-btn-3', 'name-title-3');
 myFunction('select-btn-4', 'name-title-4');
 myFunction('select-btn-5', 'name-title-5');
 myFunction('select-btn-6', 'name-title-6');
+myFunction('select-btn-7', 'name-title-7');
+myFunction('select-btn-8', 'name-title-8');
+myFunction('select-btn-9', 'name-title-9');
+
+
