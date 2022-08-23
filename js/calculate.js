eda1988da802335ca40ed6fbb6f3f65675446320
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-08-23 09:34:26 +0600

    calculate total added

--- js/calculate.js
+++ js/calculate.js
@@ -1,12 +1,12 @@
 function getInputValueById(elementID){
-  const ExpenseField = document.getElementById(elementID);
-  const ExpenseFieldString = ExpenseField.value;
+  const expenseField = document.getElementById(elementID);
+  const expenseFieldString = expenseField.value;
 
-  if(isNaN(ExpenseFieldString)){
+  if(isNaN(expenseFieldString)){
     alert("provide a valid number");
     return 0;
   }
-  const getExpenseInput = parseInt(ExpenseFieldString);
+  const getExpenseInput = parseInt(expenseFieldString);
 
   if(isNaN(getExpenseInput) || getExpenseInput < 0){
     alert("provide valid number");
@@ -27,3 +27,14 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
   
 })
 
+document.getElementById('calculate-total').addEventListener('click', function(){
+  const managerCost = getInputValueById('manager-cost-input');
+  const coachCost = getInputValueById('coach-cost-input');
+
+  const getPlayerExpenses = document.getElementById('set-player-expenses');
+  const getPlayerExpensesString = getPlayerExpenses.innerText;
+  const setPlayerExpense = parseInt(getPlayerExpensesString);
+
+  const subTotalExpenses = document.getElementById('sub-total');
+  subTotalExpenses.innerText = setPlayerExpense + managerCost + coachCost;
+})
